1cca4f523de00a63f42de972eae1a52cea79284e
25Ericcheong <ericcheongkaikit@gmail.com>
2023-02-12 13:59:47 +1000

    tried using node in built url module

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const http = require("http");
-var dt = require("./datetime");
+const dt = require("./datetime");
+const url = require("url");
 
 http
   .createServer(function (req, res) {
     res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`Today's date is ${dt.date()} <br/>`);
+    res.write(`DATE: ${dt.date()} <br/>`);
+    res.write(`URL: ${req.url} <br/>`);
+
+    const query = url.parse(req.url, true).query;
+
+    if (query && query.month && query.year) {
+      res.write(`QUERY: Month - ${query.month} Year - ${query.year} <br/>`);
+    }
+
     res.write(`That is all!`);
     res.end();
   })
