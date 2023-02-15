d7a6b8e971fdb285ecdbb998bcfb2ccba9abdade
25Ericcheong <ericcheongkaikit@gmail.com>
2023-02-15 21:32:51 +1000

    tutorial practices from w3c for fun

--- index.js
+++ index.js
@@ -2,35 +2,59 @@ const http = require("http");
 const dt = require("./datetime");
 const url = require("url");
 const fs = require("fs").promises;
+const events = require("events");
+
+const eventEmitter = new events.EventEmitter();
+
+const successfulEmitterHandler = function () {
+  console.log("Successful responds!");
+};
+
+eventEmitter.on("success", successfulEmitterHandler);
 
 http
   .createServer(function (req, res) {
-    res.writeHead(200, { "Content-Type": "text/html" });
-    res.write(`DATE: ${dt.date()} <br/>`);
-    res.write(`URL: ${req.url} <br/>`);
+    if (req.url === "./fileupload") {
+    } else {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.write(`DATE: ${dt.date()} <br/>`);
+      res.write(`URL: ${req.url} <br/>`);
 
-    const query = url.parse(req.url, true).query;
+      const query = url.parse(req.url, true).query;
 
-    if (query && query.month && query.year) {
-      res.write(`QUERY: Month - ${query.month} Year - ${query.year} <br/>`);
-    }
+      if (query && query.month && query.year) {
+        res.write(`QUERY: Month - ${query.month} Year - ${query.year} <br/>`);
+      }
 
-    fs.readFile("./files/file.html", {
-      encoding: "utf8",
-      flag: "r",
-    })
-      .then((data) => {
-        res.write(`FILE FOUND: \n`);
-        res.write(data);
-        res.write(`That is all!`);
-        res.end();
+      fs.readFile("./files/file.html", {
+        encoding: "utf8",
+        flag: "r",
       })
-      .catch((err) => {
-        res.write("ERROR: \n");
-        res.write(err.message);
-        res.end();
-        return;
-      });
+        .then((data) => {
+          res.write(`FILE FOUND: \n`);
+          res.write(data);
+
+          eventEmitter.emit("success");
+
+          res.write("<h1>Try uploading a file too!</h1>");
+          res.write(
+            '<form action="fileupload" method="post" enctype="multipart/form-data">'
+          );
+          res.write('<input type="file" name="filetoupload"><br>');
+          res.write('<input type="submit">');
+          res.write("</form>");
+
+          res.write(`That is all!`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write("ERROR: \n");
+          res.write(err.message);
+
+          res.write(`That is all!`);
+          res.end();
+        });
+    }
   })
   .listen(8080);
 
