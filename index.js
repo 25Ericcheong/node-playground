8b012ccce0ecb7ba64bc8b2e3871d0966b401aa8
25Ericcheong <ericcheongkaikit@gmail.com>
2023-02-23 22:35:01 +1000

    attempt to upload file

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const formidable = require("formidable");
 const dt = require("./datetime");
 const url = require("url");
 const fs = require("fs").promises;
@@ -15,6 +16,12 @@ eventEmitter.on("success", successfulEmitterHandler);
 http
   .createServer(function (req, res) {
     if (req.url === "./fileupload") {
+      const form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        res.write("File uploaded");
+        eventEmitter.emit("success");
+        res.end();
+      });
     } else {
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(`DATE: ${dt.date()} <br/>`);
@@ -26,34 +33,42 @@ http
         res.write(`QUERY: Month - ${query.month} Year - ${query.year} <br/>`);
       }
 
-      fs.readFile("./files/file.html", {
-        encoding: "utf8",
-        flag: "r",
-      })
-        .then((data) => {
-          res.write(`FILE FOUND: \n`);
-          res.write(data);
+      res.write("<h1>Try uploading a file too!</h1>");
+      res.write(
+        '<form action="fileupload" method="post" enctype="multipart/form-data">'
+      );
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit">');
+      res.write("</form>");
 
-          eventEmitter.emit("success");
+      // fs.readFile("./files/file.html", {
+      //   encoding: "utf8",
+      //   flag: "r",
+      // })
+      //   .then((data) => {
+      //     res.write(`FILE FOUND: \n`);
+      //     res.write(data);
 
-          res.write("<h1>Try uploading a file too!</h1>");
-          res.write(
-            '<form action="fileupload" method="post" enctype="multipart/form-data">'
-          );
-          res.write('<input type="file" name="filetoupload"><br>');
-          res.write('<input type="submit">');
-          res.write("</form>");
+      //     eventEmitter.emit("success");
 
-          res.write(`That is all!`);
-          res.end();
-        })
-        .catch((err) => {
-          res.write("ERROR: \n");
-          res.write(err.message);
+      //     res.write("<h1>Try uploading a file too!</h1>");
+      //     res.write(
+      //       '<form action="fileupload" method="post" enctype="multipart/form-data">'
+      //     );
+      //     res.write('<input type="file" name="filetoupload"><br>');
+      //     res.write('<input type="submit">');
+      //     res.write("</form>");
 
-          res.write(`That is all!`);
-          res.end();
-        });
+      //     res.write(`That is all!`);
+      //     res.end();
+      //   })
+      //   .catch((err) => {
+      //     res.write("ERROR: \n");
+      //     res.write(err.message);
+
+      //     res.write(`That is all!`);
+      //     res.end();
+      //   });
     }
   })
   .listen(8080);
